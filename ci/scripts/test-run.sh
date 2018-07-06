fd02602380d88e86a28d21d6d332195ccaec2e3a
Sai Boorlagadda <sai.boorlagadda@gmail.com>
2018-07-06 14:17:25 -0700

    Check JAVA_HOME bin for java version

--- ci/scripts/test-run.sh
+++ ci/scripts/test-run.sh
@@ -76,7 +76,11 @@ else
 fi
 
 printf "\nUsing the following JDK:"
-java -version
+if [ -n "${JAVA_HOME}" ]; then
+  ${JAVA_HOME}/bin/java -version
+else
+  java -version
+fi
 printf "\n\n"
 
 directories_file=${DEST_DIR}/artifact_directories
